8cc8ee316a55fff02535da44b7d3f9c7e7a030ea
couc <1425042285@qq.com>
2021-06-23 21:17:17 +0800

    add 3rdparty/nanovg.

--- include/RolUI/IPainter.h
+++ include/RolUI/IPainter.h
@@ -2,11 +2,31 @@
 
 #include <stdint.h>
 
+#include "Rect.h"
+
 namespace RolUI {
 
     class IPainter {
       public:
         virtual ~IPainter() {}
+
+        virtual void draw_rect(const Rect& rect) = 0;
+        virtual void draw_rect(int32_t x, int32_t y, uint32_t w, uint32_t h) = 0;
+
+        virtual void draw_roundrect(const Rect& rect, uint32_t round) = 0;
+        virtual void draw_roundrect(int32_t x, int32_t y, uint32_t w, uint32_t h, uint32_t round) = 0;
+
+        virtual void drew_ellipse(const Rect& rect) = 0;
+        virtual void draw_ellipse(int32_t x, int32_t y, uint32_t w, uint32_t h) = 0;
+
+        virtual void fill_rect(const Rect& rect) = 0;
+        virtual void fill_rect(int32_t x, int32_t y, uint32_t w, uint32_t h) = 0;
+
+        virtual void fill_roundrect(const Rect& rect, uint32_t round) = 0;
+        virtual void fill_roundrect(int32_t x, int32_t y, uint32_t w, uint32_t h, uint32_t round) = 0;
+
+        virtual void fill_ellipse(const Rect& rect) = 0;
+        virtual void fill_ellipse(int32_t x, int32_t y, uint32_t w, uint32_t h) = 0;
     };
 
 } // namespace RolUI
