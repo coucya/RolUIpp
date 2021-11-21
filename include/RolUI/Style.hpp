90ad12ad200a53d4f826f5a89a0e1cae8f9417cb
couc <1425042285@qq.com>
2021-11-21 22:17:15 +0800

    样式默认为空。

--- include/RolUI/Style.hpp
+++ include/RolUI/Style.hpp
@@ -8,13 +8,13 @@
 namespace RolUI {
 
     struct Style {
-        std::optional<Color> color = Color{0, 0, 0};
-        std::optional<std::string> font_name = "default";
-        std::optional<unsigned> font_size = 15;
-        std::optional<unsigned> round = 0;
-        std::optional<unsigned> border_width = 0;
-        std::optional<Color> border_color = Color{0, 0, 0};
-        std::optional<Color> background_color = Color{255, 255, 255, 0};
+        std::optional<Color> color;
+        std::optional<std::string> font_name;
+        std::optional<unsigned> font_size;
+        std::optional<unsigned> round;
+        std::optional<unsigned> border_width;
+        std::optional<Color> border_color;
+        std::optional<Color> background_color;
     };
 
 } // namespace RolUI
