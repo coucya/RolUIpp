f00366409c4336692f8d3e856730a4a9df8a726d
couc <1425042285@qq.com>
2022-03-01 21:39:30 +0800

    删除 Button 和 Textbox 中的无用代码。

--- include/RolUI/widgets/Button.hpp
+++ include/RolUI/widgets/Button.hpp
@@ -8,22 +8,5 @@
 namespace RolUI {
     namespace widget {
 
-        class Button : public Label {
-          public:
-            Button(Widget* parent = nullptr) noexcept;
-            Button(Widget* parent, std::string text) noexcept;
-            ~Button();
-
-          public:
-            Signal<> on_click;
-
-          private:
-            void _init() noexcept;
-            void _init_event_bind() noexcept;
-
-          private:
-            bool _is_press = false;
-        };
-
     } // namespace widget
 } // namespace RolUI
